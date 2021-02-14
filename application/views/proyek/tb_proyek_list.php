968cac510fc03ef08875ff538136b8e8639316cd
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-14 11:02:47 +0700

    layout proyek

--- application/views/proyek/tb_proyek_list.php
+++ application/views/proyek/tb_proyek_list.php
@@ -12,19 +12,19 @@
     <div class='row'>
         <div class='col-xs-12'>
             <div class='box'>
-                <div class='box-header'>
-                    <h3 class='box-title'>Nama Proyek </h3>
-                    <?= anchor('tb_proyek/create/','Create',array('class'=>'btn btn-sm btn-round btn-primary btn-sm'));?><br><br>
+                <div class='box-header'><br>
+                    <?= anchor('tb_proyek/create/','Tambah',array('class'=>'btn btn-sm btn-round btn-primary btn-sm'));?>
                     <!-- <?= anchor(site_url('tb_proyek/excel'), ' <i class="fa fa-file-excel-o"></i> Excel', 'class="btn btn-primary btn-sm"'); ?>
                     <?= anchor(site_url('tb_proyek/word'), '<i class="fa fa-file-word-o"></i> Word', 'class="btn btn-primary btn-sm"'); ?>
                     <?= anchor(site_url('tb_proyek/pdf'), '<i class="fa fa-file-pdf-o"></i> PDF', 'class="btn btn-primary btn-sm"'); ?> -->
+                    <h3 class="table-header text-center"><strong>Tabel Nama Proyek</strong></h3>
                 </div><!-- /.box-header -->
                 <div class='box-body'>
                     <table class="table table-striped" id="mytable">
                         <thead>
                             <tr>
                                 <th width="80px">No</th>
-                                <th>Nama proyek</th>
+                                <th>Nama Proyek</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
@@ -54,14 +54,6 @@
             ?>
                         </tbody>
                     </table>
-                    <script src="<?= base_url('assets/js/jquery-1.11.2.min.js') ?>"></script>
-                    <script src="<?= base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
-                    <script src="<?= base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
-                    <script type="text/javascript">
-                    $(document).ready(function() {
-                        $("#mytable").dataTable();
-                    });
-                    </script>
                 </div><!-- /.box-body -->
             </div><!-- /.box -->
         </div><!-- /.col -->
