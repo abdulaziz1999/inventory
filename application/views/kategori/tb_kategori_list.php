ea50f9ed8fe61afc0e5d982edf0a624bd65febee
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-14 11:04:42 +0700

    layout kategori

--- application/views/kategori/tb_kategori_list.php
+++ application/views/kategori/tb_kategori_list.php
@@ -12,12 +12,12 @@
     <div class='row'>
         <div class='col-xs-12'>
             <div class='box'>
-                <div class='box-header'>
-                    <h3 class='box-title'>Kategori Barang</h3>
-                    <?php echo anchor('tb_kategori/create/','Create',array('class'=>'btn btn-sm btn-round btn-primary btn-sm'));?><br><br>
+                <div class='box-header'><br>
+                    <?php echo anchor('tb_kategori/create/','Tambah',array('class'=>'btn btn-sm btn-round btn-primary btn-sm'));?>
                     <!-- <?php echo anchor(site_url('tb_kategori/excel'), ' <i class="fa fa-file-excel-o"></i> Excel', 'class="btn btn-primary btn-sm"'); ?>
-		<?php echo anchor(site_url('tb_kategori/word'), '<i class="fa fa-file-word-o"></i> Word', 'class="btn btn-primary btn-sm"'); ?>
-		<?php echo anchor(site_url('tb_kategori/pdf'), '<i class="fa fa-file-pdf-o"></i> PDF', 'class="btn btn-primary btn-sm"'); ?> -->
+                    <?php echo anchor(site_url('tb_kategori/word'), '<i class="fa fa-file-word-o"></i> Word', 'class="btn btn-primary btn-sm"'); ?>
+                    <?php echo anchor(site_url('tb_kategori/pdf'), '<i class="fa fa-file-pdf-o"></i> PDF', 'class="btn btn-primary btn-sm"'); ?> -->
+                    <h3 class="table-header text-center"><strong>Tabel Kategori Barang</strong></h3>
                 </div><!-- /.box-header -->
                 <div class='box-body'>
                     <table class="table table-striped" id="mytable">
@@ -54,14 +54,6 @@
             ?>
                         </tbody>
                     </table>
-                    <script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js') ?>"></script>
-                    <script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
-                    <script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
-                    <script type="text/javascript">
-                    $(document).ready(function() {
-                        $("#mytable").dataTable();
-                    });
-                    </script>
                 </div><!-- /.box-body -->
             </div><!-- /.box -->
         </div><!-- /.col -->
