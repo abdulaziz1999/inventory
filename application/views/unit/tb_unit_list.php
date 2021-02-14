0ce4a12b3fc9b1a3ae93e52568b147cf0dcb3ce9
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-14 10:55:05 +0700

    list layout unit

--- application/views/unit/tb_unit_list.php
+++ application/views/unit/tb_unit_list.php
@@ -4,7 +4,7 @@
             <i class="ace-icon fa fa-home home-icon"></i>
             <a href="<?= base_url('admin'); ?>">Dashboard</a>
         </li>
-        <li class="active">unit</li>
+        <li class="active">Unit</li>
     </ul>
 </div>
 <!-- Main content -->
@@ -12,19 +12,19 @@
     <div class='row'>
         <div class='col-xs-12'>
             <div class='box'>
-                <div class='box-header'>
-                    <h3 class='box-title'>Unit </h3>
-                    <?php echo anchor('tb_unit/create/','Create',array('class'=>'btn btn-sm btn-round btn-primary btn-sm'));?><br><br>
+                <div class='box-header'><br>
+                    <?php echo anchor('tb_unit/create/','Tambah',array('class'=>'btn btn-sm btn-round btn-primary btn-sm'));?>
                     <!-- <?php echo anchor(site_url('tb_unit/excel'), ' <i class="fa fa-file-excel-o"></i> Excel', 'class="btn btn-primary btn-sm"'); ?>
                     <?php echo anchor(site_url('tb_unit/word'), '<i class="fa fa-file-word-o"></i> Word', 'class="btn btn-primary btn-sm"'); ?>
                     <?php echo anchor(site_url('tb_unit/pdf'), '<i class="fa fa-file-pdf-o"></i> PDF', 'class="btn btn-primary btn-sm"'); ?> -->
+                    <h3 class="table-header text-center"><strong>Tabel Unit</strong></h3>
                 </div><!-- /.box-header -->
                 <div class='box-body'>
                     <table class="table table-striped" id="mytable">
                         <thead>
                             <tr>
                                 <th width="80px">No</th>
-                                <th>Nama unit</th>
+                                <th>Nama Unit</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
@@ -54,14 +54,6 @@
             ?>
                         </tbody>
                     </table>
-                    <script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js') ?>"></script>
-                    <script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
-                    <script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
-                    <script type="text/javascript">
-                    $(document).ready(function() {
-                        $("#mytable").dataTable();
-                    });
-                    </script>
                 </div><!-- /.box-body -->
             </div><!-- /.box -->
         </div><!-- /.col -->
