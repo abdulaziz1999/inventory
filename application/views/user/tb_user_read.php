8095f4b672444a17dcc8b856fd907141d83b6ce4
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-22 11:08:45 +0700

    view user read

--- application/views/user/tb_user_read.php
+++ application/views/user/tb_user_read.php
@@ -8,23 +8,18 @@
                     <table class="table table-bordered">
                         <tr>
                             <td>Nama User</td>
-                            <td><?php echo $nama_user; ?></td>
+                            <td><?= $nama; ?></td>
                         </tr>
                         <tr>
-                            <td>Password</td>
-                            <td><?php echo $password; ?></td>
+                            <td>Username</td>
+                            <td><?= $username; ?></td>
                         </tr>
                         <tr>
-                            <td>Hak Akses</td>
-                            <td><?php echo $hak_akses; ?></td>
+                            <td>Level</td>
+                            <td><?= $level; ?></td>
                         </tr>
-                        <tr>
-                            <td>Avatar</td>
-                            <td><?php echo $avatar; ?></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td><a href="<?php echo site_url('tb_user') ?>" class="btn btn-default">Cancel</a></td>
+                        <td></td>
+                        <td><a href="<?= site_url('tb_user') ?>" class="btn btn-default">Cancel</a></td>
                         </tr>
                     </table>
                 </div><!-- /.box-body -->
