de8b2cc9352dbe72bf3910ca59c97ef7cdd776ce
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-22 10:54:59 +0700

    view satuan read

--- application/views/satuan/tb_satuan_read.php
+++ application/views/satuan/tb_satuan_read.php
@@ -4,19 +4,19 @@
         <div class='col-xs-12'>
             <div class='box'>
                 <div class='box-header'>
-                    <h3 class='box-title'>Tb_satuan Read</h3>
+                <h3 class="table-header text-center"><strong>Read Satuan Barang</strong></h3>
                     <table class="table table-bordered">
                         <tr>
                             <td>Nama Satuan</td>
-                            <td><?php echo $nama_satuan; ?></td>
+                            <td><?= $nama_satuan; ?></td>
                         </tr>
                         <tr>
                             <td>Ket</td>
-                            <td><?php echo $ket; ?></td>
+                            <td><?= $ket; ?></td>
                         </tr>
                         <tr>
                             <td></td>
-                            <td><a href="<?php echo site_url('tb_satuan') ?>" class="btn btn-default">Cancel</a></td>
+                            <td><a href="<?= site_url('tb_satuan') ?>" class="btn btn-danger btn-round ntn-sm">Kembali</a></td>
                         </tr>
                     </table>
                 </div><!-- /.box-body -->
