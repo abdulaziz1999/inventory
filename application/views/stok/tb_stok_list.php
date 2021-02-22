d3c1676468692799f93b6fe3c20f997aa0e1adab
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-22 10:42:48 +0700

    view stok list

--- application/views/stok/tb_stok_list.php
+++ application/views/stok/tb_stok_list.php
@@ -21,64 +21,67 @@
                     <h3 class="table-header text-center"><strong>Data Stok Barang</strong></h3>
                 </div><!-- /.box-header -->
                 <div class='box-body'>
-                    <table class="table table-striped table-hover table-striped" id="mytable">
-                        <thead>
-                            <tr>
-                                <th width="80px">No</th>
-                                <th>Nama Barang</th>
-                                <th>Kategori Barang</th>
-                                <th>Harga Beli</th>
-                                <th>Harga Jual</th>
-                                <th>Stok Barang</th>
-                                <th>Minimal Stok</th>
-                                <th>Unit</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover table-striped" id="mytable">
+                            <thead>
+                                <tr>
+                                    <th width="80px">No</th>
+                                    <th>Nama Barang</th>
+                                    <th>Kategori Barang</th>
+                                    <th>Harga Beli</th>
+                                    <th>Harga Jual</th>
+                                    <th>Stok Barang</th>
+                                    <th>Minimal Stok</th>
+                                    <th>Unit</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
                             $start = 0;
                             foreach ($tb_stok_data as $tb_stok)
                             {
                                 ?>
-                            <tr>
-                                <td><?= ++$start ?></td>
-                                <td>
-                                    <?= $tb_stok->nama_barang ?>
-                                </td>
-                                <td>
-                                    <?= $tb_stok->nama_kategori ?>
-                                </td>
-                                <td><?= "Rp. ".number_format($tb_stok->harga_beli,0,"",".") ?></td>
-                                <td><?= "Rp. ".number_format($tb_stok->harga_jual,0,"",".") ?></td>
-                                <td>
-                                    <span class="badge badge-success">
-                                        <?= $tb_stok->stok." ".$tb_stok->nama_satuan ?>
-                                    </span>
-                                </td>
-                                <td>
-                                    <span class="badge badge-danger">
-                                        <?= $tb_stok->min_stok." ".$tb_stok->nama_satuan ?>
-                                    </span>
-                                </td>
-                                <td>
-                                    <span class="label label-lg label-primary arrowed-right">
-                                        <?= $tb_stok->nama_unit ?>
-                                    </span>
-                                </td>
-                                <?php 
+                                <tr>
+                                    <td><?= ++$start ?></td>
+                                    <td>
+                                        <?= $tb_stok->nama_barang ?>
+                                    </td>
+                                    <td>
+                                        <?= $tb_stok->nama_kategori ?>
+                                    </td>
+                                    <td><?= "Rp. ".number_format($tb_stok->harga_beli,0,"",".") ?></td>
+                                    <td><?= "Rp. ".number_format($tb_stok->harga_jual,0,"",".") ?></td>
+                                    <td>
+                                        <span class="badge badge-success">
+                                            <?= $tb_stok->stok." ".$tb_stok->nama_satuan ?>
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <span class="badge badge-danger">
+                                            <?= $tb_stok->min_stok." ".$tb_stok->nama_satuan ?>
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <span class="label label-lg label-primary arrowed-right">
+                                            <?= $tb_stok->nama_unit ?>
+                                        </span>
+                                    </td>
+                                    <?php 
                             // echo anchor(site_url('tb_stok/read/'.$tb_stok->id_barang),'<i class="fa fa-eye"></i>',array('title'=>'detail','class'=>'btn btn-danger btn-sm')); 
                             // echo '  '; 
                             // echo anchor(site_url('tb_stok/update/'.$tb_stok->id_barang),'<i class="fa fa-pencil-square-o"></i>',array('title'=>'edit','class'=>'btn btn-danger btn-sm')); 
                             // echo '  '; 
                             // echo anchor(site_url('tb_stok/delete/'.$tb_stok->id_barang),'<i class="fa fa-trash-o"></i>','title="delete" class="btn btn-danger btn-sm" onclick="javasciprt: return confirm(\'Are You Sure ?\')"'); 
 			                ?>
-                                </td>
-                            </tr>
-                            <?php
+                                    </td>
+                                </tr>
+                                <?php
                             }
                             ?>
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
+                    <hr>
                 </div><!-- /.box-body -->
             </div><!-- /.box -->
         </div><!-- /.col -->
