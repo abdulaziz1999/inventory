d434ba11771d2845fbb7e2a696ea13857da9e2d7
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-22 10:38:06 +0700

    view proyek list

--- application/views/proyek/tb_proyek_list.php
+++ application/views/proyek/tb_proyek_list.php
@@ -13,7 +13,7 @@
         <div class='col-xs-12'>
             <div class='box'>
                 <div class='box-header'><br>
-                    <?= anchor('tb_proyek/create/','Tambah',array('class'=>'btn btn-sm btn-round btn-primary btn-sm'));?>
+                    <?= anchor('tb_proyek/create/','<i class="fa fa-plus"></i> Tambah',array('class'=>'btn btn-sm btn-round btn-primary btn-sm'));?>
                     <!-- <?= anchor(site_url('tb_proyek/excel'), ' <i class="fa fa-file-excel-o"></i> Excel', 'class="btn btn-primary btn-sm"'); ?>
                     <?= anchor(site_url('tb_proyek/word'), '<i class="fa fa-file-word-o"></i> Word', 'class="btn btn-primary btn-sm"'); ?>
                     <?= anchor(site_url('tb_proyek/pdf'), '<i class="fa fa-file-pdf-o"></i> PDF', 'class="btn btn-primary btn-sm"'); ?> -->
