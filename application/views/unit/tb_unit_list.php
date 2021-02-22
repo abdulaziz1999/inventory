6451ed62eb5fcdd186a11849da369c5ba8fc0777
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-22 11:06:20 +0700

    view unit list

--- application/views/unit/tb_unit_list.php
+++ application/views/unit/tb_unit_list.php
@@ -13,10 +13,10 @@
         <div class='col-xs-12'>
             <div class='box'>
                 <div class='box-header'><br>
-                    <?php echo anchor('tb_unit/create/','Tambah',array('class'=>'btn btn-sm btn-round btn-primary btn-sm'));?>
-                    <!-- <?php echo anchor(site_url('tb_unit/excel'), ' <i class="fa fa-file-excel-o"></i> Excel', 'class="btn btn-primary btn-sm"'); ?>
-                    <?php echo anchor(site_url('tb_unit/word'), '<i class="fa fa-file-word-o"></i> Word', 'class="btn btn-primary btn-sm"'); ?>
-                    <?php echo anchor(site_url('tb_unit/pdf'), '<i class="fa fa-file-pdf-o"></i> PDF', 'class="btn btn-primary btn-sm"'); ?> -->
+                    <?= anchor('tb_unit/create/','<i class="fa fa-plus"></i>  Tambah',array('class'=>'btn btn-sm btn-round btn-primary btn-sm'));?>
+                    <!-- <?= anchor(site_url('tb_unit/excel'), ' <i class="fa fa-file-excel-o"></i> Excel', 'class="btn btn-primary btn-sm"'); ?>
+                    <?= anchor(site_url('tb_unit/word'), '<i class="fa fa-file-word-o"></i> Word', 'class="btn btn-primary btn-sm"'); ?>
+                    <?= anchor(site_url('tb_unit/pdf'), '<i class="fa fa-file-pdf-o"></i> PDF', 'class="btn btn-primary btn-sm"'); ?> -->
                     <h3 class="table-header text-center"><strong>Tabel Unit</strong></h3>
                 </div><!-- /.box-header -->
                 <div class='box-body'>
@@ -35,8 +35,8 @@
             {
                 ?>
                             <tr>
-                                <td><?php echo ++$start ?></td>
-                                <td><?php echo $tb_unit->nama_unit ?></td>
+                                <td><?= ++$start ?></td>
+                                <td><?= $tb_unit->nama_unit ?></td>
                                 <td style="text-align:center" width="300px">
                                     <div class="btn-group btn-corner">
                                         <?php 
