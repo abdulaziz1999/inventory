6ee33114f646a60618e66fb1d70f81586d3efceb
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-22 10:15:39 +0700

    log

---
+++ application/helpers/log_helper.php
@@ -0,0 +1,32 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+function dataLog($aktifitas){
+	$this->load->library('user_agent');
+        if ($this->agent->is_browser())
+		{
+			$agent = $this->agent->browser().' '.$this->agent->version();
+		}
+		elseif ($this->agent->is_robot())
+		{
+			$agent = $this->agent->robot();
+		}
+		elseif ($this->agent->is_mobile())
+		{
+			$agent = $this->agent->mobile();
+		}
+		else
+		{
+			$agent = 'Unidentified User Agent';
+		}
+
+        $data = [
+            'nama'      => $this->session->userdata('nama'),
+            'level'     => $this->session->userdata('level'),
+            'aktifitas' => $aktifitas,
+            'browser'   => $agent,
+            'platform'  => $this->agent->platform(),
+            'ip_address'=> $this->input->ip_address(),
+        ];
+        $this->db->insert('tb_log',$data);
+		return ;
+    }
