48176ac619f2880e66f994634e96b5cc31092144
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-22 11:07:15 +0700

    view user form

--- application/views/user/tb_user_form.php
+++ application/views/user/tb_user_form.php
@@ -7,33 +7,28 @@
 
                     <h3 class='box-title'>TB_USER</h3>
                     <div class='box box-primary'>
-                        <form action="<?php echo $action; ?>" method="post">
+                        <form action="<?= $action; ?>" method="post">
                             <table class='table table-bordered'>
                                 <tr>
-                                    <td>Nama User <?php echo form_error('nama_user') ?></td>
-                                    <td><input type="text" class="form-control" name="nama_user" id="nama_user"
-                                            placeholder="Nama User" value="<?php echo $nama_user; ?>" />
+                                    <td>Nama User <?= form_error('nama') ?></td>
+                                    <td><input type="text" class="form-control" name="nama" id="nama"
+                                            placeholder="Nama User" value="<?= $nama; ?>" />
                                     </td>
                                 <tr>
-                                    <td>Password <?php echo form_error('password') ?></td>
-                                    <td><input type="text" class="form-control" name="password" id="password"
-                                            placeholder="Password" value="<?php echo $password; ?>" />
+                                    <td>Username <?= form_error('username') ?></td>
+                                    <td><input type="text" class="form-control" name="username" id="username"
+                                            placeholder="username" value="<?= $username; ?>" />
                                     </td>
                                 <tr>
-                                    <td>Hak Akses <?php echo form_error('hak_akses') ?></td>
-                                    <td><input type="text" class="form-control" name="hak_akses" id="hak_akses"
-                                            placeholder="Hak Akses" value="<?php echo $hak_akses; ?>" />
+                                    <td>Level <?= form_error('level') ?></td>
+                                    <td><input type="text" class="form-control" name="level" id="level"
+                                            placeholder="Hak Akses" value="<?= $level; ?>" />
                                     </td>
-                                <tr>
-                                    <td>Avatar <?php echo form_error('avatar') ?></td>
-                                    <td><input type="text" class="form-control" name="avatar" id="avatar"
-                                            placeholder="Avatar" value="<?php echo $avatar; ?>" />
-                                    </td>
-                                    <input type="hidden" name="id_user" value="<?php echo $id_user; ?>" />
+                                    <input type="hidden" name="id_pengguna" value="<?= $id_pengguna; ?>" />
                                 <tr>
                                     <td colspan='2'><button type="submit"
-                                            class="btn btn-primary"><?php echo $button ?></button>
-                                        <a href="<?php echo site_url('tb_user') ?>" class="btn btn-default">Cancel</a>
+                                            class="btn btn-primary"><?= $button ?></button>
+                                        <a href="<?= site_url('tb_user') ?>" class="btn btn-default">Cancel</a>
                                     </td>
                                 </tr>
 
