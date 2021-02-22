f7a7aca53e10ac18d7eae957f65cafb8e8c99284
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-22 10:31:17 +0700

    view issuing rad

--- application/views/issuing/tb_issuing_read.php
+++ application/views/issuing/tb_issuing_read.php
@@ -1,31 +1,45 @@
+<div class="breadcrumbs ace-save-state" id="breadcrumbs">
+    <ul class="breadcrumb">
+        <li>
+            <i class="ace-icon fa fa-home home-icon"></i>
+            <a href="<?= base_url('admin'); ?>">Dashboard</a>
+        </li>
+        <li>
+            <i class="ace-icon fa fa-truck"></i>
+            <a href="<?= base_url('tb_issuing'); ?>">Issuing</a>
+        </li>
+        <li class="active">Read</li>
+    </ul>
+</div>
 <!-- Main content -->
 <section class='content'>
     <div class='row'>
         <div class='col-xs-12'>
             <div class='box'>
                 <div class='box-header'>
-                    <h3 class='box-title'>Tb_issuing Read</h3>
+                    <h3 class="table-header text-center"><strong>Read</strong></h3>
                     <table class="table table-bordered">
                         <tr>
                             <td>Tgl</td>
-                            <td><?php echo $tgl; ?></td>
+                            <td><?= $tgl; ?></td>
                         </tr>
                         <tr>
                             <td>No Invoice</td>
-                            <td><?php echo $no_ref; ?></td>
+                            <td><?= $no_ref; ?></td>
                         </tr>
                         <tr>
                             <td>Picker</td>
-                            <td><?php echo $picker; ?></td>
+                            <td><?= $picker; ?></td>
                         </tr>
                         <tr>
                             <td>Remarks</td>
-                            <td><?php echo $remarks; ?></td>
+                            <td><?= $remarks; ?></td>
                         </tr>
                         <tr>
-                            <td></td>
-                            <td><a href="<?php echo site_url('tb_issuing') ?>"
-                                    class="btn btn-sm btn-round btn-default">Cancel</a></td>
+                            <td colspan="2">
+                                <a href="<?= site_url('tb_issuing') ?>" class="btn btn-sm btn-round btn-danger">
+                                    <i class="fa fa-reply"></i> Kembali</a>
+                            </td>
                         </tr>
                     </table>
                 </div><!-- /.box-body -->
