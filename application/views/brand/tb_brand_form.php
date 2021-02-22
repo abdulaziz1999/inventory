9ce230989e6b6ae2c02a9d9b5f798d2292568820
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-22 10:28:54 +0700

    view brand form

--- application/views/brand/tb_brand_form.php
+++ application/views/brand/tb_brand_form.php
@@ -5,7 +5,7 @@
             <div class='box'>
                 <div class='box-header'>
 
-                    <h3 class='box-title'>TB_BRAND</h3>
+                <h3 class="table-header text-center"><strong>Tambah Brand Barang</strong></h3>
                     <div class='box box-primary'>
                         <form action="<?= $action; ?>" method="post">
                             <table class='table table-bordered'>
