a8af30da88a870f183805f7c8aadb48190da93b8
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-22 10:36:19 +0700

    view pemesan list

--- application/views/pemesan/tb_pemesan_list.php
+++ application/views/pemesan/tb_pemesan_list.php
@@ -13,7 +13,7 @@
         <div class='col-xs-12'>
             <div class='box'>
                 <div class='box-header'><br>
-                    <?= anchor('tb_pemesan/create/','Tambah',array('class'=>'btn btn-sm btn-round btn-primary btn-sm'));?>
+                    <?= anchor('tb_pemesan/create/','<i class="fa fa-plus"></i> Tambah',array('class'=>'btn btn-sm btn-round btn-primary btn-sm'));?>
                     <!-- <?= anchor(site_url('tb_pemesan/excel'), ' <i class="fa fa-file-excel-o"></i> Excel', 'class="btn btn-primary btn-sm"'); ?>
                     <?= anchor(site_url('tb_pemesan/word'), '<i class="fa fa-file-word-o"></i> Word', 'class="btn btn-primary btn-sm"'); ?>
                     <?= anchor(site_url('tb_pemesan/pdf'), '<i class="fa fa-file-pdf-o"></i> PDF', 'class="btn btn-primary btn-sm"'); ?> -->
