9d89ad5cef92f141b5e80ed13975970ffa8e52e8
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-22 10:39:52 +0700

    view receiving create

--- application/views/receiving/tb_receiving_create.php
+++ application/views/receiving/tb_receiving_create.php
@@ -1,3 +1,16 @@
+<div class="breadcrumbs ace-save-state" id="breadcrumbs">
+    <ul class="breadcrumb">
+        <li>
+            <i class="ace-icon fa fa-home home-icon"></i>
+            <a href="<?= base_url('admin'); ?>">Dashboard</a>
+        </li>
+        <li>
+            <i class="ace-icon fa fa-shopping-cart"></i>
+            <a href="<?= base_url('tb_receiving'); ?>">Receiving</a>
+        </li>
+        <li class="active">Tambah</li>
+    </ul>
+</div>
 <!-- Main content -->
 <section class='content'>
     <div class='row'>
@@ -5,36 +18,70 @@
             <div class='box'>
                 <div class='box-header'>
 
-                    <h3 class='box-title'>TB_RECEIVING</h3>
+                    <h3 class="table-header text-center"><strong>Buat Data Barang Masuk</strong></h3>
                     <div class='box box-primary'>
-                        <form action="<?php echo $action; ?>" method="post">
-                            <table class='table table-bordered'>
+                        <form action="<?= $action; ?>" method="post">
+                            <table class='table table-bordered table-hover table-striped'>
                                 <tr>
-                                    <td>Tgl <?php echo form_error('tgl') ?></td>
+                                    <td>Tanggal <?= form_error('tgl') ?></td>
                                     <td><input type="date" class="form-control" name="tgl" id="tgl" placeholder="Tgl"
-                                            value="<?php echo $tgl; ?>" />
+                                            required autocomplete="off" value="<?= $tgl; ?>" />
                                     </td>
                                 <tr>
-                                    <td>No PO <?php echo form_error('no_ref') ?></td>
-                                    <td><input type="text" class="form-control" name="no_ref" id="no_ref"
-                                            placeholder="No Ref" value="<?php echo $no_ref; ?>" />
+                                    <td>No PO <?= form_error('no_ref') ?></td>
+                                    <td><input type="text" class="form-control" name="no_ref" id="no_ref" required
+                                            autocomplete="off" placeholder="No Ref" value="<?= $no_ref; ?>" />
                                     </td>
                                 <tr>
-                                    <td>Supplier <?php echo form_error('supplier') ?></td>
-                                    <td><input type="text" class="form-control" name="supplier" id="supplier"
-                                            placeholder="Supplier" value="<?php echo $supplier; ?>" />
+                                    <td>Supplier <?= form_error('supplier') ?></td>
+                                    <td>
+                                        <select class="form-control" name="supplier" id="supplier" required>
+                                            <option value="" selected disabled>----Pilih Nama Supplier----</option>
+                                            <?php foreach($nm_suplier->result() as $s):?>
+                                            <option <?php if($supplier == $s->id_suplier) { echo 'selected';}?>
+                                                value="<?= $s->id_suplier?>"><?= $s->nama_suplier?></option>
+                                            <?php endforeach;?>
+                                        </select>
                                     </td>
                                 <tr>
-                                    <td>Pemesan <?php echo form_error('remarks') ?></td>
-                                    <td><input type="text" class="form-control" name="remarks" id="remarks"
-                                            placeholder="Remarks" value="<?php echo $remarks; ?>" />
+                                    <td>Pemesan <?= form_error('remarks') ?></td>
+                                    <td>
+                                        <select class="form-control" name="remarks" id="remarks" required>
+                                            <option value="" selected disabled>----Pilih Nama Pemesan----</option>
+                                            <?php foreach($nm_pemesan->result() as $s):?>
+                                            <option <?php if($supplier == $s->id_pemesan) { echo 'selected';}?>
+                                                value="<?= $s->id_pemesan?>"><?= $s->nama_pemesan?></option>
+                                            <?php endforeach;?>
+                                        </select> 
                                     </td>
-                                    <input type="hidden" name="id_receiving" value="<?php echo $id_receiving; ?>" />
+                                    <input type="hidden" name="id_receiving" value="<?= $id_receiving; ?>" />
                                 <tr>
-                                    <td colspan='2'><button type="submit"
-                                            class="btn btn-primary"><?php echo $button ?></button>
-                                        <a href="<?php echo site_url('tb_receiving') ?>"
-                                            class="btn btn-default">Cancel</a>
+                                    <td>Nama Proyek <?= form_error('nama_proyek') ?></td>
+                                    <td>
+                                        <select class="form-control" name="nama_proyek" id="nama_proyek" required>
+                                            <option value="" selected disabled>----Pilih Nama Proyek----</option>
+                                            <?php foreach($nm_proyek->result() as $s):?>
+                                            <option <?php if($nama_proyek == $s->id_proyek) { echo 'selected';}?>
+                                                value="<?= $s->id_proyek?>"><?= $s->nama_proyek?></option>
+                                            <?php endforeach;?>
+                                        </select>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Keterangan <?= form_error('ket') ?></td>
+                                    <td><input type="text" class="form-control" name="ket" id="ket"
+                                            placeholder="Keterangan" value="<?= $ket; ?>" />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan='2'>
+                                        <div class="btn-group btn-corner">
+                                            <button type="submit" class="btn btn-sm btn-primary"
+                                                style="margin-right:3px;"><i class="fa fa-plus"></i>
+                                                <?= $button ?></button>
+                                            <a href="<?= site_url('tb_receiving') ?>" class="btn btn-sm btn-danger">
+                                                <i class="fa fa-reply"></i> Kembali</a>
+                                        </div>
                                     </td>
                                 </tr>
                             </table>
