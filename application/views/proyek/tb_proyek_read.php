cc1dd6c00d4a0f7d393c106d581baa2555f5e114
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-22 10:38:44 +0700

    view proyek read

--- application/views/proyek/tb_proyek_read.php
+++ application/views/proyek/tb_proyek_read.php
@@ -4,7 +4,7 @@
         <div class='col-xs-12'>
             <div class='box'>
                 <div class='box-header'>
-                    <h3 class='box-title'>Tb_proyek Read</h3>
+                <h3 class="table-header text-center"><strong>Read Nama Proyek</strong></h3>
                     <table class="table table-bordered">
                         <tr>
                             <td>Nama proyek</td>
@@ -12,7 +12,7 @@
                         </tr>
                         <tr>
                             <td></td>
-                            <td><a href="<?= site_url('tb_proyek') ?>" class="btn btn-default">Cancel</a></td>
+                            <td><a href="<?= site_url('tb_proyek') ?>" class="btn btn-sm btn-danger btn-round">Kembali</a></td>
                         </tr>
                     </table>
                 </div><!-- /.box-body -->
