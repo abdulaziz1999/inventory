eb8ebf1d37f2abe361bb232a32f80bc15e59908e
abdulaziz1999 <azizmentor96@gmail.com>
2021-02-24 08:27:51 +0700

    update template

--- application/views/template.php
+++ application/views/template.php
@@ -701,8 +701,10 @@
                 text: ''
             },
             xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
-                    'Nov', 'Dec'
+                categories: [
+                    <?php foreach($chart_rev as $row):?>
+                        '<?= date('M',strtotime($row->tgl))?>',
+                    <?php endforeach;?>
                 ]
             },
             yAxis: {
@@ -720,10 +722,18 @@
             },
             series: [{
                 name: 'Penjualan',
-                data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+                data: [
+                    <?php foreach($chart_iss as $row):?>
+                            <?=$row->total?>,
+                    <?php endforeach;?>
+                ]
             }, {
                 name: 'Pembelian',
-                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+                data: [
+                    <?php foreach($chart_rev as $row):?>
+                            <?=$row->total?>,
+                    <?php endforeach;?>
+                ]
             }],
 
             responsive: {
