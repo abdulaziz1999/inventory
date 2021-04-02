b6ed8178852479fb28b97b77c659b9ab6ab9f56e
abdulaziz1999 <azizmentor96@gmail.com>
2021-04-03 05:47:57 +0700

    template update

--- application/views/template.php
+++ application/views/template.php
@@ -684,7 +684,7 @@
                                     <div  class="bg-white p-2" style="border:1px solid #000;padding:10px;"> 
                                             <?= "<img alt='Barcode Generator TEC-IT' src='https://barcode.tec-it.com/barcode.ashx?data=" . $kode . "&code=EAN13&multiplebarcodes=true&translate-esc=true&unit=Fit&dpi=96&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&qunit=Mm&quiet=0' />" ?><br>
                                     </div>
-                                        <h5 class="text-center mb-1 mt-2" style="color:#000;">` + nama + `</h5>
+                                        <h5 class="text-center mb-1 mt-2" style="color:#000;"><strong>` + nama + `</strong></h5>
                                         <h5 class="text-center font-weight-normal mb-1" style="color:#000;">Rp. <?= rupiah($harga) ?></h5>
                                     </div>
                                 </div>
