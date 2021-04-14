09e90b145268c19291e094468fe521341d6dbf05
abdulaziz1999 <azizmentor96@gmail.com>
2021-04-14 09:33:06 +0700

    disable export

--- application/views/stok/tb_stok_list.php
+++ application/views/stok/tb_stok_list.php
@@ -16,8 +16,8 @@
                     <br>
                     <!--  //echo anchor('tb_stok/create/','Create',array('class'=>'btn btn-danger btn-sm'));?> -->
                     <!-- <?= anchor(site_url('tb_stok/pdf'), '<i class="fa fa-file-pdf-o"></i> PDF', 'class="btn btn-sm btn-round btn-warning btn-sm" target="_blank"'); ?><br> -->
-                     <?= anchor(site_url('tb_stok/excel'), ' <i class="fa fa-file-excel-o"></i> Excel', 'class="btn btn-primary btn-sm"'); ?>
-                    <?= anchor(site_url('tb_stok/word'), '<i class="fa fa-file-word-o"></i> Word', 'class="btn btn-primary btn-sm"'); ?>
+                     <!-- <?= anchor(site_url('tb_stok/excel'), ' <i class="fa fa-file-excel-o"></i> Excel', 'class="btn btn-primary btn-sm"'); ?> -->
+                    <!-- <?= anchor(site_url('tb_stok/word'), '<i class="fa fa-file-word-o"></i> Word', 'class="btn btn-primary btn-sm"'); ?> -->
                     <h3 class="table-header text-center"><strong>Data Stok Barang</strong></h3>
                 </div><!-- /.box-header -->
                 <div class='box-body'>
