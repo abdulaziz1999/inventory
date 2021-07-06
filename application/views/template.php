b682d7c862281e8b0c7f5e23bdf7f03391d011dc
abdulaziz1999 <azizmentor96@gmail.com>
2021-07-06 07:00:19 +0700

    update template

--- application/views/template.php
+++ application/views/template.php
@@ -216,7 +216,7 @@
 
                     <b class="arrow"></b>
                 </li>
-            <?php if($this->session->userdata('level') == 'admin'){?>
+            <?php if($this->session->userdata('level') == 'admin' || $this->session->userdata('level') == 'staff'){?>
                 <li class="<?php if($this->uri->segment(1) == 'tb_kategori'){ echo "active open"; }
                     elseif($this->uri->segment(1) == 'tb_brand'){ echo "active open"; }
                     elseif($this->uri->segment(1) == 'tb_satuan'){ echo "active open"; }
@@ -329,7 +329,7 @@
                     </a>
                     <b class="arrow"></b>
                 </li>
-            <?php if($this->session->userdata('level') == 'admin'){?>
+            <?php if($this->session->userdata('level') == 'admin' || $this->session->userdata('level') == 'staff'){?>
                 <li
                     class="<?php if($this->uri->segment(1) == 'tb_stok' && $this->uri->segment(2) == ''){ echo "active"; }else{ echo "";}?>">
                     <a href="<?= base_url('tb_stok'); ?>">
