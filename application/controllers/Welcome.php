65f390b3c05d6cdf735b6c107a3842023a8341cf
abdulaziz1999 <azizmentor96@gmail.com>
2021-12-01 09:10:08 +0700

    Mode Maintenance

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,29 +20,47 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->library('user_agent');
+		// $this->load->library('user_agent');
 
-		if ($this->agent->is_browser())
-		{
-			$agent = $this->agent->browser().' '.$this->agent->version();
-		}
-		elseif ($this->agent->is_robot())
-		{
-			$agent = $this->agent->robot();
-		}
-		elseif ($this->agent->is_mobile())
-		{
-			$agent = $this->agent->mobile();
-		}
-		else
-		{
-			$agent = 'Unidentified User Agent';
-		}
+		// if ($this->agent->is_browser())
+		// {
+		// 	$agent = $this->agent->browser().' '.$this->agent->version();
+		// }
+		// elseif ($this->agent->is_robot())
+		// {
+		// 	$agent = $this->agent->robot();
+		// }
+		// elseif ($this->agent->is_mobile())
+		// {
+		// 	$agent = $this->agent->mobile();
+		// }
+		// else
+		// {
+		// 	$agent = 'Unidentified User Agent';
+		// }
 
-		echo $agent;
-		echo "<br>";
-		echo $this->agent->platform(); 
-		echo "<br>";
-		echo $this->input->ip_address(); 
+		// echo $agent;
+		// echo "<br>";
+		// echo $this->agent->platform(); 
+		// echo "<br>";
+		// echo $this->input->ip_address(); 
+	}
+
+	public function maintenance(){
+		?>
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Mode Maintenance</title>
+		</head>
+		<body align="center">
+			<h1>Maintenance Mode</h1>
+			<img src="<?= base_url('assets/images/maintenance.gif')?>" alt="" srcset="">
+		</body>
+		</html>
+		<?php
 	}
 }
