fd46ae72cd37c653c899e544c1b5fdeb01e89986
abdulaziz1999 <azizmentor96@gmail.com>
2022-04-03 22:16:52 +0700

    update laporan card

--- application/controllers/Laporan_card.php
+++ application/controllers/Laporan_card.php
@@ -16,6 +16,10 @@ class Laporan_card extends CI_Controller{
     function index(){
         $start = $this->input->get('s', TRUE);
         $end = $this->input->get('e', TRUE);
+        $idc = $this->input->get('idc', TRUE);
+        $i = $this->input->get('i', TRUE);
+        $k = $this->input->get('k', TRUE);
+        $u = $this->input->get('u', TRUE);
         $data['kategori']   = @$this->db->get('tb_kategori');
         $data['unit']       = @$this->db->get('tb_unit');
         $data['barang']     = @$this->db->select('id_barang,nama_barang')->get('tb_barang');
@@ -25,6 +29,19 @@ class Laporan_card extends CI_Controller{
         }else{
             $start = date('Y-m-d h:i:s');
             $end = date('Y-m-d h:i:s');
+        }
+        if($idc){
+            $this->db->join('tb_stok st','tb_barang.id_barang = st.id_barang');
+            $this->db->where('cutoff_id', $idc);
+            if($i == TRUE){
+                $this->db->where('tb_barang.id_barang =', $i);
+            }elseif($k == TRUE){
+                $this->db->where('kategori =', $k);
+            }elseif($u == TRUE){
+                $this->db->where('unit_id =', $u);
+            }
+            //sum per id barang
+            $data['stok'] =	$this->db->select('*')->get('tb_barang');
         }
 
         $this->template->load('template', 'laporan/laporan_kartuStok_cutoff',$data);
